3335fe0d6c41c8b98bb3cf3fc19a0c9275745e7c
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-03-28 14:25:34 +0200

    create function to convert GML files into shafiles

--- load_data_utils.py
+++ load_data_utils.py
@@ -1,5 +1,6 @@
 from owslib.wfs import WebFeatureService
-from data_informations import services, data_wfs
+import geopandas as gpd
+import os
 
 def connection_wfs(url, service_name, version):
     """ Return a wfs object after connecting to a service thanks the url provided """
@@ -33,3 +34,29 @@ def load_data(url, service_name, version, data_list, path):
         file = open(f"{path}/{d_name}.gml", "wb")
         file.write(new_data.read())
         file.close()
+
+def gml_to_shapefile(input_path, output_path, folder=False):
+    """Convert GML file into shapefile.
+    If folder = TRUE, all of the GML files of the given folder are converted to shapefile. 
+    Careful about the path : either a folder or a file one. 
+    """
+    print("converting ... ")
+    if(folder):
+        for filename in os.listdir(input_path):
+            print(f"filename : {filename}")
+            if filename.endswith('.gml'):
+                # Read input GML file into a GeoDataFrame
+                gdf = gpd.read_file(os.path.join(input_path, filename))
+                
+                # Set output file path and name
+                output_name = filename.replace('.gml', '.shp')
+                new_output_path = os.path.join(output_path, output_name)
+                
+                # Write GeoDataFrame to output Shapefile
+                gdf.to_file(new_output_path, driver='ESRI Shapefile')
+    else:
+
+        gdf = gpd.read_file(input_path)
+        gdf.to_file(output_path, driver='ESRI Shapefile')
+
+    print("Done, all GML files converted into Shapefile")

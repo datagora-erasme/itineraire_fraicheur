22ba04e481c96c1e771bd0d8b5ae9ea8bfa392f0
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-03-29 11:51:22 +0200

    use json file into all needed function

--- download_data.py
+++ download_data.py
@@ -1,13 +1,15 @@
-from data_utils import download_data, gml_to_shapefile, points_to_polygon
-from data_informations import services, data_wfs
+from data_utils import download_data, convert_all_gml_data_to_shapefile, points_to_polygon
+#from data_informations import services, data_wfs
 import geopandas as gpd
+
 print("#### LOADING ALL DATA #### \n \n")
 
-download_data(services["data.grandlyon_wfs"], "data.grandlyon_wfs", "2.0.0", data_wfs, "./data/gml")
+download_data("data.grandlyon_wfs", "2.0.0", "./data/gml")
 
 print("#### Converting GML into Shapefile #### \n \n")
 
-gml_to_shapefile("./data/gml/", "./data/shp/", folder=True)
+convert_all_gml_data_to_shapefile("./data/shp/")
 
-print("#### Converting Points Shapefile into Polygons #### \n \n")
+# print("#### Converting Points Shapefile into Polygons #### \n \n")
+
 

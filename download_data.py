0fe946d9365be0bbb3a6a90372e1623ebb57c3a2
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-03-29 17:35:28 +0200

    correct function to convert Points into Polygons : need to change the CRS + write function to convert many files + create function to create folder

--- download_data.py
+++ download_data.py
@@ -1,15 +1,17 @@
-from data_utils import download_data, convert_all_gml_data_to_shapefile, points_to_polygon
+import os
+from data_utils import download_data, convert_all_gml_data_to_shapefile, create_folder,convert_all_points_into_polygons
 #from data_informations import services, data_wfs
 import geopandas as gpd
 
 print("#### LOADING ALL DATA #### \n \n")
-
+create_folder("./data/gml")
 download_data("data.grandlyon_wfs", "2.0.0", "./data/gml")
 
 print("#### Converting GML into Shapefile #### \n \n")
-
+create_folder("./data/shp")
 convert_all_gml_data_to_shapefile("./data/shp/")
 
-# print("#### Converting Points Shapefile into Polygons #### \n \n")
-
+print("#### Converting Points Shapefile into Polygons #### \n \n")
+create_folder("./data/shp_buffered")
+convert_all_points_into_polygons("./data/shp_buffered/")
 

f4425b0c5da7ba2abdf76ff6b15932c74f5260c9
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-05-09 10:18:19 +0200

    remove warnings for web app

--- pocwa-init/src/App.js
+++ pocwa-init/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import NavBar from './components/navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 
 function App() {

2f9c55e92e5312d21c6a67cca65f25e692636505
Yannis-Barba <yannis.barba.dev@gmail.com>
2023-06-01 14:24:05 +0200

    created the headband experimentation component

---
+++ frontend/src/components/headband.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HeadBand(){
+    return(
+        <div style={{zIndex:1000}} className="absolute bg-red-500 w-full h-[3vh]">
+            Expérimentation <a href="#">[texte formulaire]</a>
+        </div>
+    )
+
+}
+
+export default HeadBand;
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -4,10 +4,12 @@ import Map from '../components/map'
 
 import Content from '../components/content';
 import BackButton from '../components/backButton';
+import HeadBand from '../components/headband';
 
 function Home(){
     return (
         <div style={{position: 'relative'}} className="min-h-screen max-h-screen">
+            <HeadBand/>
             <BackButton/>
             <Content/>
             <Map/>
